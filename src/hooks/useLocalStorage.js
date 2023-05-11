75022aae8d9a79a17978cc9491fa079ff9e901ba
Irem Nur Gozudok <iremgozudok99@icloud.com>
2023-05-11 17:35:56 +0300

    local storage hook'u olusturuldu

---
+++ src/hooks/useLocalStorage.js
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+
+export default function useLocalStorage (key, defaultValue) {
+    
+    const [value, setValue] = useState(() => {
+        const jsonValue = localStorage.getItem(key)
+        console.log(jsonValue)
+        if (jsonValue) return JSON.parse(jsonValue)
+
+        if(typeof defaultValue === "function") {
+            return defaultValue()
+        } else {
+           return defaultValue
+        }
+    })
+
+    useEffect(() => {
+        localStorage.setItem(key, JSON.stringify(value))
+    }, [key, value])
+
+    return [value, setValue]
+} 
